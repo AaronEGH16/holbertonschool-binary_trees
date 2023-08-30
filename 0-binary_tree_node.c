14b965904b5a7feea4458af4f2f5903d202b0a35
ghp_SqFSGexg5fuxb0zIxm5n05k0BUgwA02v693m <ghp_SqFSGexg5fuxb0zIxm5n05k0BUgwA02v693m>
2023-08-30 23:53:44 +0000

    header file and function to create a new tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - create a new node to the tree
+ *
+ * @parent: parent of the node
+ * @value: value of the node content
+ *
+ * Return: the address of the new node or NULL in failure
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node;
+
+    new_node = malloc(sizeof(binary_tree_t));
+    if (!new_node)
+        return (NULL);
+
+    new_node->parent = parent;
+    new_node->n = value;
+    new_node->left = NULL;
+    new_node->right = NULL;
+
+    return (new_node);
+}
