d961e5d8853e6dd50df66aba50ce9cd70ee35c8b
ghp_SqFSGexg5fuxb0zIxm5n05k0BUgwA02v693m <ghp_SqFSGexg5fuxb0zIxm5n05k0BUgwA02v693m>
2023-08-31 15:50:50 +0000

    function to execute func in traversal order

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder -
+ * binary_tree_preorder - execute func command in traversal order
  *
- * @tree:
- * @func:
+ * @tree: address of the tree
+ * @func: function to execute
 */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (!tree || !func)
-        return;
+	if (!tree || !func)
+		return;
 
-    binary_tree_preorder(tree->left, func((tree->left)->n));
-    binary_tree_preorder(tree->right, func((tree->right)->n));
-    return (func());
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
