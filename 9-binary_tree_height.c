e041e07f717a4034529843ad93f18bcd53a3b0bc
ghp_SqFSGexg5fuxb0zIxm5n05k0BUgwA02v693m <ghp_SqFSGexg5fuxb0zIxm5n05k0BUgwA02v693m>
2023-08-31 16:34:09 +0000

    proto of function

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height -
+ *
+ * @tree: address of the root to check
+ *
+ * Return: size of tree
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+
+}
