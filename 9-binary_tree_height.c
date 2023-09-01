24a14b9559d0e75b723c27df6c5d48c3c49e88f7
ghp_SqFSGexg5fuxb0zIxm5n05k0BUgwA02v693m <ghp_SqFSGexg5fuxb0zIxm5n05k0BUgwA02v693m>
2023-09-01 12:48:54 +0000

    function to get the height of the tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,7 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height -
+ * max - return max between two sizes
+ *
+ * @a: size of a
+ * @b: size of b
+ *
+ * Return: MAX
+*/
+
+size_t max(size_t a, size_t b)
+{
+	if (a >= b)
+		return (a);
+	else
+		return (b);
+}
+
+/**
+ * binary_tree_height - get the height of the tree
  *
  * @tree: address of the root to check
  *
@@ -10,5 +27,14 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t n;
 
+	if (!tree || (!(tree->left) && !(tree->right)))
+		return (0);
+
+	n = 1;
+
+	n += max(binary_tree_height(tree->left), binary_tree_height(tree->right));
+
+	return (n);
 }
